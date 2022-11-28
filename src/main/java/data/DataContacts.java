154aa31f68ea420bc63ec029452daaa211ae0912
lachhab nacer <lachhab.nacer@gmail.com>
2022-11-28 01:16:39 +0100

    webService REST, Deployement sur Tocat avec API jersey[les jar dans lib] et descripteur de deployement contenant la ServletContainer de jersey[context de l'application]

---
+++ src/main/java/data/DataContacts.java
@@ -0,0 +1,11 @@
+package data;
+
+public class DataContacts {
+
+	public static Contact[] listContacts= {
+			new Contact(0, "Bouslamti", "Ahmed"),
+			new Contact(1, "ElKamel", "Karim"),
+			new Contact(2, "Mouradi", "Yassir"),
+			new Contact(3,"Mouradi","Ayman"),
+			new Contact(4,"Nadir","Ali")};
+}
